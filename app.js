cae2f07ac048882460bc727794e9054dfbc4e3d3
unknown <jailsonp437@gmail.com>
2021-12-05 19:19:12 -0400

    Home page created

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
     const session = require('express-session')
     const flash = require('connect-flash')
     const mongoose = require('mongoose')
+    require("./models/Postagem")
+    const Postagem = mongoose.model("Postagem")
 
 //Configurações
     //Body-Parser
@@ -43,9 +45,34 @@
 
 
 // //Rotas
-    app.get('/', (req, res) => {
-        res.send('Página inicial')
+    app.get("/", (req,res) => {
+        Postagem.find().populate("categoria").sort({data: "desc"}).then((postagens) => {
+            res.render("index", {postagens: postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
     })
+
+    app.get("/postagem/:slug", (req, res) => {
+        Postagem.findOne({slug: req.params.slug}).then((postagem) => {
+            if(postagem){
+                res.render("postagem/index", {postagem: postagem})
+            } else{
+                req.flash("error_msg", "Esta postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
+
+    app.get("/404", (req,res) => {
+        res.send("Erro 404!")
+    })
+
     app.get('/posts', (req, res) => {
         res.send('Lista de posts')
     })
