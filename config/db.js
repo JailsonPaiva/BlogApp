54940e3f324e65b1c3ccb1fbf61bf0217cf7baca
Jailson Paiva <j.psherl1009@gmail.com>
2021-12-11 12:57:26 -0400

    finished 03

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production") {
-	module.exports = {mongoURI: "mongodb+srv://jailson_admin:admin123@cluster0.sy9ps.mongodb.net/Cluster0?retryWrites=true&w=majority"}
+	module.exports = {mongoURI: "mongodb+srv://jailson:12345@blogapp.sy9ps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 } else {
 	module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
