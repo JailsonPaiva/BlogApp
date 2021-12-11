7b97d092ff0defdcecf3dc672ceca4e3e07ca6fb
unknown <jailsonp437@gmail.com>
2021-12-11 12:13:09 -0400

    finished 01

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production") {
-	module.exports = {mongoURI: "mongodb+srv://jailson_admin:admin123@cluster0.sy9ps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
+	module.exports = {mongoURI: "mongodb+srv://jailson_admin:admin123@cluster0.sy9ps.mongodb.net/cluster0-shard-00-02.sy9ps.mongodb.net:27017?retryWrites=true&w=majority"}
 } else {
 	module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
